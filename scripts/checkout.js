5e5669c68f6d6fda794bb868b32bac7069f7b0f0
nagi0705 <bellreuben1126@gmail.com>
2024-08-27 12:02:26 +0900

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart,removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -29,7 +29,7 @@ cart.forEach((cartItem) => {
             ${matchingProduct.name}
           </div>
           <div class="product-price">
-            $${formatCurrency(matchingProduct.priceCents)}
+            ${formatCurrency(matchingProduct.priceCents)}
           </div>
           <div class="product-quantity">
             <span>
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -75,7 +75,7 @@ cart.forEach((cartItem) => {
           </div>
           <div class="delivery-option">
             <input type="radio" class="delivery-option-input" 
-            name="delivery-${matchingProduct.id}">
+            name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
@@ -91,5 +91,14 @@ cart.forEach((cartItem) => {
   `;
 });
 
+document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+            console.log(cart);
+        });
+    });
