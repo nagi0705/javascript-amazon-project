9e7a896bce2864334a5495542fa64d517851f41a
nagi0705 <bellreuben1126@gmail.com>
2024-08-29 20:45:56 +0900

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,22 +1,6 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import '../data/cart-oop.js';
 
-document.addEventListener('DOMContentLoaded', () => {
     renderOrderSummary();
     renderPaymentSummary();
-
-    
-    document.querySelectorAll('.js-update-link').forEach((button) => {
-        button.addEventListener('click', () => {
-            const productId = button.dataset.productId;
-            
-            console.log(`Update clicked for product ID: ${productId}`);
-            
-            const newQuantity = prompt('Enter new quantity:');
-            if (newQuantity && !isNaN(newQuantity)) {
-                
-                console.log(`New quantity for product ID ${productId}: ${newQuantity}`);
-            }
-        });
-    });
-});
