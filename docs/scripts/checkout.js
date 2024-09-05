0baed630914185739f2b7d7ae6ee582dc7f6cf7b
nagi0705 <bellreuben1126@gmail.com>
2024-09-05 13:47:21 +0900

    a

--- docs/scripts/checkout.js
+++ docs/scripts/checkout.js
@@ -1,16 +1,14 @@
 // checkout.js
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { loadProductsFetch } from '/javascript-amazon-project/data/products.js'; // パスを絶対パスに変更
-import { loadCart } from '/javascript-amazon-project/data/cart.js'; // パスを絶対パスに変更
+import { loadProductsFetch } from '/javascript-amazon-project/data/products.js';
+import { loadCart } from '/javascript-amazon-project/data/cart.js';
 
-// ページの読み込み処理
 async function loadPage() {
     try {
-        // 商品データを非同期に読み込む
+        
         await loadProductsFetch();
 
-        // カートデータを非同期に読み込む
         await new Promise((resolve) => {
             loadCart(() => {
                 resolve();
