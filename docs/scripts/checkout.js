5477366f3f22653daa7434852ca6c259f18e69f9
nagi0705 <bellreuben1126@gmail.com>
2024-09-05 12:41:15 +0900

    Fix paths in checkout.js

--- docs/scripts/checkout.js
+++ docs/scripts/checkout.js
@@ -1,5 +1,5 @@
-import { renderOrderSummary } from './checkout/orderSummary.js';
-import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { renderOrderSummary } from '../scripts/checkout/orderSummary.js';
+import { renderPaymentSummary } from '../scripts/checkout/paymentSummary.js';
 import { loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 
